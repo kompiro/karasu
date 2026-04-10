45d042fcc053510f25aa65d6f4075ec0e0e25109
Hiroki Kondo <kompiro@gmail.com>
2026-04-10 08:25:55 +0000

    fix(cli): remove unused FmtOptions export

--- packages/cli/src/fmt.ts
+++ packages/cli/src/fmt.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { format, FormatError } from "@karasu-tools/core";
 
-export interface FmtOptions {
+interface FmtOptions {
   check?: boolean;
   stdin?: boolean;
 }
